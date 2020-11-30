800c6c6e490edfc7a0f47cfd1b6d1669c8e4ec4c
짱유나 <60169666+zzanguna@users.noreply.github.com>
2020-12-01 05:33:33 +0900

    Revert "엑셀 내보내기 기능 구현 전 update"

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,8 @@
 # coding=utf-8
 """
     Created by JungYunAh Hello!
-    ProjectName : InstagramCommentProject
-    Date        : 2020/12/01 3:41 오전
+    ProjectName : InstagramCommentScrapping
+    Date        : 2020/11/30 1:31 오후
     Notion      : https://www.notion.so/a68b9e1475634c1389447ef8f34111b4?v=de124104486840189d382bb67efcffb4
     Github      : http://github.com/zzanunada
     Description :
